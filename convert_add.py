64d5c11ee027b572a3adf3f235ab7182f044c3e1
Gustavo Diaz Saavedra <gadiazsaavedra@gmail.com>
2022-11-24 15:09:51 -0300

    convert_add con 3 soluciones

--- convert_add.py
+++ convert_add.py
@@ -1,7 +1,14 @@
-def convert_add(**kwargs):
-    """Converts a string to a float and adds it to the total"""
-    return sum(float(value) for value in kwargs.values())
-
 list1 = ["1", "2", "3"]
-print(convert_add(**list1))
+for i in range(len(list1)):
+    list1[i] = int(list1[i])
+print("1er.metodo : ", sum(list1))
 
+print("2do.metodo : ", sum(list(map(int, list1))))
+
+
+def convert_add(*argv) -> None:
+    """Converts a string to a float and adds it to the total"""
+    return sum(int(arg) for arg in argv)
+
+
+print("3er.metodo : ", convert_add(*list1))
